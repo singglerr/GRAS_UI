c8ff4ceea37b50c2f2a2c52069d8a21fb4ef096d
Danil Usov <singglerr@gmail.com>
2020-06-15 01:43:03 +0500

    dmp model template

--- src/views/risk-module/dmp/Model.vue
+++ src/views/risk-module/dmp/Model.vue
@@ -1,7 +1,9 @@
 <template>
     <v-container fluid>
         <v-row class="pl-12 pr-12 pt-6">
-            <v-col>
+            <v-col
+                    cols="3"
+            >
                 <v-text-field
                         type="number"
                         v-model="$store.state.dmp.n"
@@ -12,8 +14,34 @@
             <v-spacer></v-spacer>
             <v-btn @click="click" color="primary" :loading="loading">Рассчитать</v-btn>
         </v-row>
-        <v-row :hidden="hidden">
-            asdonasdnasdasdnl
+        <v-row :hidden="hidden" justify="center">
+            <v-tabs centered class="ml-4 mr-4" center-active>
+                <v-tabs-slider></v-tabs-slider>
+
+                <v-tab>
+                    Таблица результата
+                </v-tab>
+                <v-tab-item>
+                    <v-row justify="center">
+                        <v-col cols="10">
+                            <v-data-table
+                                    :headers="headers"
+                                    :items="dataEx"
+                                    disable-sort
+                                    disable-filtering
+                                    dense
+                            >
+                            </v-data-table>
+                        </v-col>
+                    </v-row>
+                </v-tab-item>
+                <v-tab>
+                    Граф
+                </v-tab>
+                <v-tab-item>
+                    Граф
+                </v-tab-item>
+            </v-tabs>
         </v-row>
     </v-container>
 </template>
@@ -24,10 +52,22 @@
         data: () => ({
             loading: false,
             hidden: true,
+            headers: [
+                {text: "Номер\n шага", value: "index", align: "start", width: 110},
+            ],
+            dataEx: [
+                {index: 1},
+            ],
+            statesCount: 0,
+            strategiesCount: 0,
         }),
+        created() {
+            this.statesCount = this.$store.state.dmp.states.length;
+            this.strategiesCount = this.$store.state.dmp.strategies.length;
+        },
         methods: {
             integer(val) {
-                return !val.toString().includes(".") || "Введите целое число больше 0";
+                return !this.needInt(val) || "Введите целое число";
             },
 
             notEmptyField(val) {
@@ -35,21 +75,86 @@
             },
 
             click() {
-                if (this.$store.state.dmp.n.toString().includes(".") || this.$store.state.dmp.n === 0) {
-                    return alert("Число шагов моделирования должно быть целым больше 0!");
+                let alertText = this.getValidationAlert();
+                if (alertText) {
+                    return alert(alertText);
                 }
 
                 this.loading = true;
-                this.compute();
+                this.process();
                 this.showResult()
             },
 
-            compute() {
+            process() {
+                this.generateHeader();
+                //TODO вычисление дмп
 
+
+            },
+
+            generateHeader() {
+                const states = this.$store.state.dmp.states;
+                states.forEach(state => {
+                    this.headers.push({
+                        text: `V[${state.name}]`,
+                        value: `V${state.id}`,
+                        width: 50,
+                        align: "center"
+                    })
+                });
+
+                states.forEach(state => {
+                    this.headers.push({
+                        text: `D[${state.name}]`,
+                        value: `D${state.id}`,
+                        align: "center"
+                    })
+                });
             },
 
             showResult() {
                 this.hidden = false;
+                this.loading = false;
+            },
+
+            drawGraph() {
+                //TODO draw graph dmp
+            },
+
+            getValidationAlert() {
+                if (this.needStates()) {
+                    return "Сначала добавьте состояния!";
+                }
+
+                if (this.needStrategies()) {
+                    return "Сначала добавьте стратегии!"
+                }
+
+                if (this.needInt(this.$store.state.dmp.n)) {
+                    return "Число шагов моделирования должно быть целым!"
+                }
+
+                if (this.needGreaterZero()) {
+                    return "Число шагов моделирования должно быть целым больше 0!"
+                }
+
+                return false;
+            },
+
+            needInt(val) {
+                return val.toString().includes(".");
+            },
+
+            needGreaterZero() {
+                return this.$store.state.dmp.n == 0;
+            },
+
+            needStates() {
+                return this.statesCount === 0;
+            },
+
+            needStrategies() {
+                return this.strategiesCount === 0;
             }
         }
     }
